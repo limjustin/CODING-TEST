342170879ef466e61cc5c95640236dd0fdae8096
Jaeyoung Lim <limjyjustin@gmail.com>
2024-03-15 23:31:18 +0900

    feat: LeetCode 그래프 탐색 문제 풀이 종료

---
+++ src/leetcode/L_841/Main.java
@@ -0,0 +1,19 @@
+package leetcode.L_841;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        List<List<Integer>> rooms = new ArrayList<>();
+        rooms.add(List.of(1));
+        rooms.add(List.of(2));
+        rooms.add(List.of(3));
+        rooms.add(List.of());
+
+
+        System.out.println(solution.canVisitAllRooms(rooms));
+    }
+}
